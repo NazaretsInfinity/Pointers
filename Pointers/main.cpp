d45d67f346e7b9e4811f1ea5a25ec18fab9880fc
Nazarets <ksenianazarec995@mail.com>
2024-04-22 23:04:51 +0300

    transfer parameters by value

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
+#define pointers_basics
 void main()
 {
 	setlocale(LC_ALL, "Russia");
-
+#ifdef pointers_basics1
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl; //взятие адресса переменной а прямо при выводе
+	cout << pa << endl; //вывод адреса переменной а , хранящегося в указатели pa
+	cout << *pa << endl; //dereference of the 'pa' pointer
+	int* pb;
+	int b = 3;
+	pb = &b;
+	cout << pb << endl;
+	cout << *pb << endl;
+#endif // pointers_basics1
+	const int n = 5;
+	short arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
 }
---
+++ transferparameters/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void exchange(int a, int b);
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	exchange(a, b);
+	cout << a << "\t" << b << endl;
+}
+void exchange(int a, int b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+}
